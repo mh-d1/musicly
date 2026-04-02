7c905f507f558c35258a9405450d8b24a8796c5e
Mn <mmhdhnfii@gmail.com>
2026-04-02 08:23:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -33,6 +33,12 @@ let songs = [
     src: "kotaini.mp3",
     cover: "cover2.jpg" 
   },
+  {
+    title: "teenagers",
+    artist: "Pixabay",
+    src: "teenagers.mp3",
+    cover: "cover2.jpg" 
+  },
 ];
 
 let currentIndex = 0;
